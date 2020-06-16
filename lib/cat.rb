c71be2e58b2786cc67583a73f29e326fe0684376
Zoe Keller <keller.zoe98@gmail.com>
2020-06-16 11:33:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,9 @@ class Cat
   attr_accessor :mood
 	attr_reader :name
 
-  def initialize(name, owner)
+  def initialize(name, new_owner)
     @name = name
-    @owner = owner 
+    @owner = new_owner 
   end
 
   def owner 
