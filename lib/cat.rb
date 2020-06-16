8483d118ff501e450b1c375c8700d9f174ddfd47
Zoe Keller <keller.zoe98@gmail.com>
2020-06-16 11:12:48 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+	attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @@owner = hermione 
+  end
+
+  def mood
+    @mood
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
-class Owner
-  # code goes here
+class Owner 
+  
+  attr_reader :species 
+  
+  @@owners = []
+  
+  def initialize(name, species=("human"))
+    @name=name 
+    @species=species 
+    @@owners << self 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  
+  def say_species
+    "I am a #{@species}."
+  end
+
+   def self.all
+    @@owners
+  end
+  
+    def self.count
+      @@owners.length 
+    end
+  
+  def self.reset_all
+    self.all.clear
+  end
+
+   def buy_cat(cat_name)
+    pets[:cats] << Cat.new(cat_name)
+  end
+  
+  def buy_dog(dog_name)
+    pets[:dogs] << Dog.new(dog_name)
+  end
+  
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+   def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+        end
+      animals.clear
+    end
+  end
+  
+  def list_pets
+    "I have #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
+  
+  
 end
