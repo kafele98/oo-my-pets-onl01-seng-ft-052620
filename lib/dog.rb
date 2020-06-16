617d1ae140800764c57dd9b0a502858cee00a24a
Zoe Keller <keller.zoe98@gmail.com>
2020-06-16 11:32:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :mood
-  attr_reader :name, :owner 
+  attr_reader :name
 
   def initialize(name, owner)
     @name = name
-    @owner = human
+    @owner = owner 
   end
 end
